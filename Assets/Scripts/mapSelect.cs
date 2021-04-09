aae626d19b11a23fdc967ae33f8955c9ad673223
natalia257 <38053456+natalia257@users.noreply.github.com>
2021-04-09 20:49:28 +0200

    Created core game

---
+++ Assets/Scripts/mapSelect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mapSelect : MonoBehaviour
+{
+    public GameObject StartPanel;
+
+    public void SetMapPhoto(Image Photo)
+    {
+        for(int i = 0; i < 16; i++)
+        {
+            GameObject.Find("/Foreground/puzzle " + i).transform.Find("Background").GetComponent<SpriteRenderer>().sprite = Photo.sprite;
+        }
+        StartPanel.SetActive(false);
+    }
+}
